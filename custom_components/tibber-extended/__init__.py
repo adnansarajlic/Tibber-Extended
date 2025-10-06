056c7ffec7eb5985094fd10f4be663f8e0be9ca1
adnansarajlic <adnansarajlic@gmail.com>
2025-10-06 21:55:14 +0200

    Update __init__.py

--- custom_components/tibber-extended/__init__.py
+++ custom_components/tibber-extended/__init__.py
@@ -15,6 +15,10 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data[DOMAIN][entry.entry_id] = entry.data
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    
+    # Registrera update listener för att hantera options changes
+    entry.async_on_unload(entry.add_update_listener(update_listener))
+    
     return True
 
 
@@ -26,3 +30,8 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
+
+
+async def update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Handle options update."""
+    await hass.config_entries.async_reload(entry.entry_id)
