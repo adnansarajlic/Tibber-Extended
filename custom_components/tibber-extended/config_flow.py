33296a99e8e52a2db9e16cbdb29359bc27ac337d
adnansarajlic <adnansarajlic@gmail.com>
2025-10-07 04:04:42 +0200

    Update config_flow.py

--- custom_components/tibber-extended/config_flow.py
+++ custom_components/tibber-extended/config_flow.py
@@ -152,7 +152,7 @@ class TibberExtendedOptionsFlow(config_entries.OptionsFlow):
 
     def __init__(self, config_entry):
         """Initialize options flow."""
-        self.config_entry = config_entry
+        super().__init__(config_entry)
 
     async def async_step_init(self, user_input=None) -> FlowResult:
         """Manage the options."""
